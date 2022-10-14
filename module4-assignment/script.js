77eb2872b49c699bb56f5053ceb555edd47ec4e4
Sourav Sharma <96438502+Sourav3655@users.noreply.github.com>
2022-10-14 16:10:43 +0530

    Update script.js

--- module4-assignment/script.js
+++ module4-assignment/script.js
@@ -1,6 +1,6 @@
 (function () {
 
-var names = ["Yaakov", "John", "Jourav", "Jhon", "Parul", "Frank", "Larry", "Paula", "Lara", "Jimmy"];
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 for (var i = 0; i < names.length; i++) {
   var firstLetter = names[i].charAt(0).toLowerCase();
